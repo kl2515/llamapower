d853eac380f81c363b041f29ece3058743105290
kl2515 <kl2515@rit.edu>
2016-09-13 01:43:41 -0400

    llamapower
    site

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function q1() {
+    document.getElementById("q1").innerHTML = "LLAMA";
+}
+
+function q2() {
+    document.getElementById("q2").innerHTML = "LLAMA";
+}
+
+function q3() {
+    document.getElementById("q3").innerHTML = "LLAMA";
+}
+
+function q4() {
+	document.getElementById("q4").innerHTML = "ALPACA";
+}
+
+function q5() {
+	document.getElementById("q5").innerHTML = "ALPACA";
+}
